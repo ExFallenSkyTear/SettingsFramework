5d76bf05718d53fbc8f92e13caecf0ae1ca3085f
Ex <atomic.theck@gmail.com>
2022-09-01 17:26:41 +0200

    Initial Commit

--- src/com/java/framework/settings/Category.java
+++ src/com/java/framework/settings/Category.java
@@ -1,27 +1,38 @@
 package com.java.framework.settings;
 
-import java.util.HashMap;
+import java.util.ArrayList;
 
 public class Category {
-    private HashMap<String, GenericEntry> categoryEntries = new HashMap<>();
+    private ArrayList<GenericEntry> categoryEntries = new ArrayList<>();
     private final String name;
 
     Category(String name) {
         this.name = name;
     }
 
+    public String getName() {
+        return this.name;
+    }
+
     public <E extends GenericEntry> E createEntry(final Class<E> clazz, String entryName) throws Exception {
         E newEntry = clazz.newInstance();
         newEntry.setName(entryName);
-        categoryEntries.put(entryName, newEntry);
+        categoryEntries.add(newEntry);
         return newEntry;
     }
 
     public <E extends GenericEntry> E getEntry(String entryName) {
-        return (E) categoryEntries.get(entryName);
+        return (E) categoryEntries.get(getEntryIndex(entryName));
     }
 
-    public String getName() {
-        return this.name;
+    public boolean entryExist(String entryName) {
+        return getEntryIndex(entryName) >= 0;
+    }
+
+    private int getEntryIndex(String entryName) {
+        for (int i = 0; i < categoryEntries.size(); i++) {
+            if (categoryEntries.get(i).getName() == entryName) return i;
+        }
+        return -1;
     }
 }
--- src/com/java/framework/settings/SettingsManager.java
+++ src/com/java/framework/settings/SettingsManager.java
@@ -1,17 +1,29 @@
 package com.java.framework.settings;
 
-import java.util.HashMap;
+import java.util.ArrayList;
 
 public class SettingsManager {
-    private HashMap<String, Category> settingsCategories = new HashMap<>();
+    private ArrayList<Category> settingsCategories = new ArrayList<>();
 
-    public Category createCategory(String categoryName) {
+    public Category createCategory(String categoryName) throws IllegalArgumentException {
+        if (categoryExist(categoryName)) throw new IllegalArgumentException();
         Category newCategory = new Category(categoryName);
-        settingsCategories.put(categoryName, newCategory);
+        settingsCategories.add(newCategory);
         return newCategory;
     }
 
     public Category getCategory(String categoryName) {
-        return settingsCategories.get(categoryName);
+        return settingsCategories.get(getCategoryIndex(categoryName));
+    }
+
+    public boolean categoryExist(String categoryName) {
+        return getCategoryIndex(categoryName) >= 0;
+    }
+
+    private int getCategoryIndex(String categoryName) {
+        for (int i = 0; i < settingsCategories.size(); i++) {
+            if (settingsCategories.get(i).getName() == categoryName) return i;
+        }
+        return -1;
     }
 }
